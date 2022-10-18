82cf88dee7e0866d6b494618435fdaffe59bbe65
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-10-18 11:33:04 -0700

    update art of the possible to generate a single table to add big n and footnotes

--- code/05_Art-of-the-Possible.R
+++ code/05_Art-of-the-Possible.R
@@ -2,108 +2,42 @@
 
 library(tidyverse)
 library(haven)
-
-pft_tbl <- read_xpt("datasets/ARD/pft.xpt")
-primary_tbl <- read_xpt("datasets/ARD/model.xpt")
-
-## From an ARD, create a plot --------------------------------------------------
-plot_dat <- pft_tbl %>%
-  filter(
-    row_label2 == "FEV1 (L)",
-    param %in% c("mean", "sd", "n"),
-    col1 %in% c("GSK123456 100 mg", "Placebo")
-  ) %>%
-  mutate(
-    row_label3  = as.numeric(factor(row_label3 , levels = c("Baseline", "Week 4", "Week 8", "Week 12")))
-  ) %>%
-  select(
-    Visit = row_label3,
-    treatment = col1,
-    param,
-    value
-  ) %>%
-  pivot_wider(
-    names_from = param,
-    values_from = value
-  ) %>%
-  mutate(
-    se = sd/sqrt(n)
-  )
-
-ggplot(
-  data = plot_dat,
-  aes(x = Visit,
-      y = mean,
-      color = treatment,
-      group = treatment)
-  ) +
-  geom_point(,
-    size = 3
-  ) +
-  geom_line(
-    size = 1
-  ) +
-  geom_errorbar(
-    aes(
-      ymin = mean + se,
-      ymax = mean - se
-    ),
-    width = 0.1,
-    size = 1
-  ) +
-  geom_label(
-    data = filter(plot_dat, Visit == 4),
-    aes(
-      label = treatment,
-      x = Visit + 0.2,
-      y = mean + 0.08
-    ),
-    hjust = 1
-  ) +
-
-  # ggplot styling
-  scale_color_manual(values = c("#4DAF4A", "#377EB8")) +
-  theme_bw() +
-  labs(y = "FEV1 (L)\nMean ± SE") +
-  theme_bw() +
-  theme(legend.position = "none",panel.grid.minor.x = element_blank()) +
-  scale_x_continuous(
-    limits = c(.5, 4.5),
-    expand = c(0, 0),
-    breaks = c(1, 2, 3, 4),
-    labels = c("Baseline","Week 4", "Week 8", "Week 12")
-  )
-
-
-## Build a Primary Results Table
 library(tfrmt)
 library(gt)
 library(gtExtras)
 
-### Define ARD columns of importance ------------------------------------------
+# ARD Created
+primary_tbl <- read_xpt("datasets/ARD/model.xpt") %>%
+  mutate(across(everything(),function(x){x[x==""]<-NA;x}))
 
-## Sort out which columns exist, and what they contain
+# Build a Primary Results Table ------------------------------------------------
+
+## Define ARD columns of importance -------------------------------------------
+
+### Sort out which columns exist, and what they contain
+
+head(primary_tbl)
 
 primary_results_tfrmt <- tfrmt(
-  group = "pfts",
-  column = c(span_col, lower_col),
+  group = model_results_category,
+  label = measure,
+  column = c(visit, trt),
   param = param,
-  label = VISIT,
-  value = "value",
+  value = value,
   sorting_cols = ord1,
 )
 
-## NOTE: Since we do not know which columns are actually in the data, if you
-## have an object defined in your environment, when you create a tfrmt that
-## requires a column name that is the same as the object, use quotes. otherwise
-## it will use the objects values:
+# NOTE: Since we do not know which columns are actually in the data, if you
+# have an object defined in your environment, when you create a tfrmt that
+# requires a column name that is the same as the object, use quotes. otherwise
+# it will use the objects values:
 
 fake_obj <- "132456"
 
 tfrmt(column = fake_obj)$column
 tfrmt(column = "fake_obj")$column
 
-### Define Body Plan -----------
+## Define Body Plan - Basics  -----------
 
 # - frmt_structure defines:
 #    - Group, Row, and format to apply
@@ -112,47 +46,185 @@ tfrmt(column = "fake_obj")$column
 #      - frmt()
 #      - frmt_combine()
 
+primary_tbl %>%
+  distinct(param)
 
 primary_tbl %>%
-  dplyr::group_by(VISIT) %>%
+  dplyr::filter(param != "big_n") %>%
+  dplyr::group_by(trt, measure) %>%
   dplyr::summarise(
     param_grp = paste(unique(param), collapse = ", ")
-  ) %>%
-  dplyr::group_by(param_grp) %>%
-  dplyr::summarise(
-    VISIT = paste(unique(VISIT), collapse = ", ")
   )
+
+# Single param into a single cell
+frmt_structure(
+  group_val = ".default",
+  label_val = ".default",
+  estimate = frmt("x.xxxx")
+)
+
+frmt_structure(
+  group_val = ".default",
+  label_val = "Week 4", ## special rounding for week 4 row
+  estimate = frmt("x.xxxxx")
+)
+
+# Combining params into a single cell
+frmt_structure(
+  group_val = ".default",
+  label_val = ".default",
+  frmt_combine("[{conf.low}, {conf.high}]",
+               conf.low = frmt("x.xxxx"),
+               conf.high = frmt("x.xxxx")
+  ))
+
+primary_results_tfrmt <- primary_results_tfrmt %>%
+  tfrmt(
+    body_plan = body_plan(
+
+      ## by default round all values to 2
+      frmt_structure(
+        group_val = ".default",
+        label_val = ".default",
+        frmt("x.xx")
+      ),
+
+      ## For all group "Model Estimates", and labels Adjusted
+      ## Mean/SE apply rounding to 4 decimals and 5 decimals respectively
+      frmt_structure(
+        group_val = "Model Estimates",
+        label_val = "Adjusted Mean",
+        estimate = frmt("x.xxxx")
+      ),
+      frmt_structure(
+        group_val = "Model Estimates",
+        label_val = "SE",
+        std.error = frmt("x.xxxxx")
+      ),
+
+      ## For group value of "Contrast", and label value of
+      ## "Difference", round to 4 decimals
+      frmt_structure(
+        group_val = "Contrast",
+        label_val = "Difference",
+        estimate = frmt("x.xxxx")
+      ),
+
+      ## For group value of "Contrast", and label value of
+      ## "95% CI [high, low]", combine `conf.low` and `conf.high` together,
+      ## rounding to 4 decimals
+      frmt_structure(
+        group_val = "Contrast",
+        label_val = "95% CI [high, low]",
+        frmt_combine("[{conf.low}, {conf.high}]", frmt("x.xxxx"))
+      )
+    )
+  )
+
+
+## Define Body Plan - Conditional Formatting  ----------
+
+#  What about the p.value param?
+#
+#  Formatting may depend on the value. More than just rounding, but
+#  commonly pvalues are limited to when values are less than 0.05 to
+#  just displaying "<0.05", or adding asterisks to significant values, etc
+#
+#  Conditional Formatting with frmt's with frmt_when!
+#  ?frmt_when
+
+# Left side evaluates comparing against _input_ value to format.
+# Right side is the frmt or output to be applied to the input value
+
+conditional_frmt <- frmt_when(
+  ">=10" ~ frmt("xx.x"),
+  ">=1" ~ frmt("x.x"),
+  "<1" ~ frmt("x.xx **"),
+  "TRUE" ~ "MISSING VALUE"
+)
+
+# Preview how the frmt_when may impact the content
+apply_frmt(
+  frmt_def = conditional_frmt,
+  .data = tibble::tibble(x = c(11,9,2,.005,NA)),
+  value = rlang::quo(x)
+)
 
 
 primary_results_tfrmt <- primary_results_tfrmt %>%
   tfrmt(
-  body_plan = body_plan(
-
-    ## For all groups and labels, combine `conf.low` and `conf.high` together, rounding to 4 decimals
-    frmt_structure(group_val = ".default", label_val = ".default",
-                   frmt_combine("[{conf.low}, {conf.high}]",
-                                frmt("x.xxxx"))),
-
-    ## For all groups and labels, apply rounding to 4 decimals to estimate, and 5 decimals to std.error
-    frmt_structure(group_val = ".default", label_val = ".default", estimate = frmt("x.xxxx")),
-    frmt_structure(group_val = ".default", label_val = ".default", std.error = frmt("x.xxxxx")),
-
-    ## For all groups and labels, conditionally format p.value such that
-    ## when the value is less than .001, display "<0.001", when the
-    ## value is greater than .99, display ">0.99", and otherwise round to
-    ## 3 decimals
-    frmt_structure(group_val = ".default", label_val = ".default",
-                   p.value = frmt_when(
-                     "<0.001" ~ "<0.001",
-                     ">0.99" ~ ">0.99",
-                     TRUE ~ frmt("x.xxx")
-                   )
+    body_plan = body_plan(
+      ## For all groups and labels, conditionally format p.value such that
+      ## when the value is less than .001, display "<0.001", when the
+      ## value is greater than .99, display ">0.99", and otherwise round to
+      ## 3 decimals
+      frmt_structure(
+        group_val = "Contrast",
+        label_val = "p-value",
+        p.value = frmt_when(
+          "<0.001" ~ "<0.001",
+          ">0.99" ~ ">0.99",
+          TRUE ~ frmt("x.xxx")
+          )
+      )
     )
   )
-)
 
 
-### Define Row Group Plan -----------------------------------------------------
+## Define "Big N's" ----------------------------------------------------------
+
+# we know they are included in the ARD, so what are they?
+
+primary_tbl %>%
+  dplyr::filter(param == "big_n")
+
+
+primary_results_tfrmt <- primary_results_tfrmt %>%
+  tfrmt(
+    ## define "big N" dressings. Values from s
+    big_n = big_n_structure(
+      param_val = "big_n",
+      n_frmt = frmt("\n(N=XX)")
+    )
+  )
+
+## Define col_plan -----------------------------------------------------------
+
+# We need to define the column order for which we want things to appear.
+# by default all columns (between column columns and actual columns in ARD)
+# are preserved. To drop non-defined columns, set ".drop" in in col_plan to TRUE.
+#
+# The column plan takes unquoted columns (can also optionally pass) as quoted.
+#
+# Behavior is _similar_ to dplyr::select, but goes with "last identified" model
+# as opposed to "first identified" that tidyselect does. Renaming works similarly.
+#
+# If you want to define column orders for spanning header content, use the `span_structure()`
+# function. This expects the argname to be the original column name then the
+# values are a vector. Renaming uses named vectors.s
+#
+
+primary_tbl %>% filter(param != "big_n") %>% distinct(visit, trt)
+primary_tbl %>% colnames
+
+
+primary_results_tfrmt <- primary_results_tfrmt %>%
+  tfrmt(
+    ## Define order of columns
+    col_plan = col_plan(
+      model_results_category,
+      measure,
+      span_structure(
+        visit = c(`Week 4`, `Week 8`, `Week 12`),
+        trt = c(`Placebo`,`GSK123456 100 mg`)
+      ),
+      -pfts,
+      -starts_with("ord")
+    )
+  )
+
+
+## Define Row Group Plan -----------------------------------------------------
 
 # Some formatting is related to spacing around groups and row label placement
 #
@@ -173,64 +245,62 @@ primary_results_tfrmt <- primary_results_tfrmt %>%
   )
 
 
-### Define col_plan -----------------------------------------------------------
+## Define footnote plan -----------------------------------------------------
 
-# We need to define the column order for which we want things to appear.
-# by default all columns (between column columns and actual columns in ARD)
-# are preserved. To drop non-defined columns, set ".drop" in in col_plan to TRUE.
+#  -`footnote_plan()` defines the set of footnotes to be added, containing 1 or more
+# `footnote_structure()`, and the mark type to use.
 #
-# The column plan takes unquoted columns (can also optionally pass) as quoted.
-#
-# Behavior is _similar_ to dplyr::select, but goes with "last identified" model
-# as opposed to "first identified" that tidyselect does. Renaming works similarly.
-#
-# If you want to define column orders for spanning header content, use the `span_structure()`
-# function. This expects the argname to be the original column name then the
-# values are a vector. Renaming uses named vectors.s
-#
-
-primary_tbl %>% distinct(span_col, lower_col)
-
+#  -`footnote_structure()` is passed to define
+#     - The footnote text
+#     - Location of the footnote based on group, label, and columns
+#         - specifying one of group, label,  column puts it in the row/column labels
+#         - specifying multiple puts it into the table cell
+# set up a structure for blocking based on the groups
+# collapse the groups with label and indent
 
 primary_results_tfrmt <- primary_results_tfrmt %>%
   tfrmt(
-    ## Define order of columns
-    col_plan = col_plan(
-      pfts,
-      VISIT,
-      span_structure(
-        span_col = c(`GSK123456 100 mg`, Placebo),
-        lower_col = c(`Adjusted Mean`, `(SE)`)
+    footnote_plan = footnote_plan(
+      footnote_structure(
+        "Estimates based on MMRM using an unstructured correlation matrix and allowing distinct variance for each visit",
+        group_val = list(model_results_category = c("Model Estimates","Contrast"))
       ),
-      span_structure(
-        span_col = `GSK123456 100 mg - Placebo`,
-        lower_col = c(Difference, `[95% CI]`, pValue = `p-value`)
+      footnote_structure(
+        "Contrasts based on pairwise contrast method with no adjustment",
+        group_val = list(model_results_category = "Contrast"),
+        label_val = list(measure = "p-value")
       ),
-      -starts_with("ord")
+      footnote_structure(
+        "Special footnote to demo calling out a value",
+        column_val = list(visit = "Week 12")
+      )
     )
   )
 
 
+
+# Generate Table ------------------
+
 primary_gt <- print_to_gt(primary_results_tfrmt, primary_tbl)
 
-### Layering new Body Plan Components-------------------------------------------
+## New Body Plan Components-------------------------------------------
 
 ### Apply additional styling as needed, say for using scientific notation for
 ### small p-values
 
-
 primary_results_tfrmt_alt <- primary_results_tfrmt %>%
-
   tfrmt(
   # new formatting for p-values
   body_plan = body_plan(
-    frmt_structure(group_val = ".default", label_val = ".default",
-                   p.value = frmt_when(
-                     ## styling
-                     "<0.001" ~ frmt("x.xxx", scientific = "x10^xx"),
-                     ">0.99" ~ ">0.99",
-                     TRUE ~ frmt("x.xxx")
-                   )
+    frmt_structure(
+      group_val = "Contrast",
+      label_val = "p-value",
+      p.value = frmt_when(
+        ## styling
+        "<0.001" ~ frmt("x.xxx", scientific = "x10^xx"),
+        ">0.99" ~ ">0.99",
+        TRUE ~ frmt("x.xxx")
+        )
     )
   )
 )
@@ -248,8 +318,8 @@ primary_gt_alt_styled <- primary_gt_alt %>%
       style = "italic"
     ),
     locations = cells_body(
-      columns = contains('pValue'),
-      rows = grepl("WEEK 12", x = VISIT)
+      columns = contains('Week 12'),
+      rows = grepl("p-value", x = measure)
     )
   )
 
--- dataset_code/make_ard.R
+++ dataset_code/make_ard.R
@@ -9,154 +9,171 @@ library(stringr)
 library(tidyr)
 
 adsl <- read_xpt("datasets/ADAM/adsl.xpt")
-adae <- read_xpt("datasets/ADAM/adae.xpt") 
-adpft <- read_xpt("datasets/ADAM/adpft.xpt") %>% 
+adae <- read_xpt("datasets/ADAM/adae.xpt")
+adpft <- read_xpt("datasets/ADAM/adpft.xpt") %>%
   mutate(VISIT = factor(VISIT, levels = c("BASELINE", "WEEK 4", "WEEK 8", "WEEK 12")))
 
 # Demog -------------------------------------------------------------------
-demog <- adsl %>% 
+demog <- adsl %>%
   tplyr_table(TRT01P, where= SAFFL =="Y") %>%
-  add_total_group() %>% 
+  add_total_group() %>%
   add_layer(
     group_count(SEX, vars("Sex", "n (%)"))
-  ) %>% 
+  ) %>%
   add_layer(
-    group_desc(AGE, by = "Age (years)") 
-  ) %>% 
+    group_desc(AGE, by = "Age (years)")
+  ) %>%
   add_layer(
     group_count(AGEGR1, by = vars("Age (years)", "n (%)") )
-  ) %>% 
+  ) %>%
   add_layer(
     group_count(RACE, by = vars("Race", "n (%)"))
-  ) %>% 
+  ) %>%
   add_layer(
-    group_count(ETHNIC, by = vars("Ethnicity", "n (%)")) 
-  ) %>% 
+    group_count(ETHNIC, by = vars("Ethnicity", "n (%)"))
+  ) %>%
   add_layer(
-    group_desc(HEIGHTBL, by = "Height (cm)") 
-  ) %>% 
+    group_desc(HEIGHTBL, by = "Height (cm)")
+  ) %>%
   add_layer(
-    group_desc(WEIGHTBL, by = "Weight (kg)") 
-  ) %>% 
+    group_desc(WEIGHTBL, by = "Weight (kg)")
+  ) %>%
   add_layer(
     group_desc(BMIBL, by = "BMI (kg/m^2)")
-  ) %>% 
-  get_numeric_data()  %>% 
-  mutate(row_label3 = if_else(row_label3 == "n (%)", row_label1, row_label3) %>% 
+  ) %>%
+  get_numeric_data()  %>%
+  mutate(row_label3 = if_else(row_label3 == "n (%)", row_label1, row_label3) %>%
            str_to_title(),
-         row_label3 = str_indent_wrap(row_label3, width = 30, tab_width = 10) %>% 
+         row_label3 = str_indent_wrap(row_label3, width = 30, tab_width = 10) %>%
            str_replace("\\\n", "\n    "),
          value = if_else(param == "pct", value*100, value),
-         ord1 = case_when(row_label2 == "Sex" ~ 1, 
-                          row_label2 == "Age (years)" ~ 2, 
-                          row_label2 == "Race" ~ 3, 
-                          row_label2 == "Ethnicity" ~ 4, 
-                          row_label2 == "Height (cm)" ~ 5, 
-                          row_label2 == "Weight (kg)" ~ 6, 
-                          row_label2 == "BMI (kg/m^2)" ~ 7, 
-                          ), 
-         ord2 =  case_when(row_label3 == "n" ~ 1, 
-                           row_label3 == "Mean (SD)" ~ 2, 
-                           row_label3 == "Median" ~ 3, 
-                           row_label3 == "Q1, Q3" ~ 4, 
-                           row_label3 == "Min, Max" ~ 5, 
-                           row_label3 == "Missing" ~ 6, 
+         ord1 = case_when(row_label2 == "Sex" ~ 1,
+                          row_label2 == "Age (years)" ~ 2,
+                          row_label2 == "Race" ~ 3,
+                          row_label2 == "Ethnicity" ~ 4,
+                          row_label2 == "Height (cm)" ~ 5,
+                          row_label2 == "Weight (kg)" ~ 6,
+                          row_label2 == "BMI (kg/m^2)" ~ 7,
+                          ),
+         ord2 =  case_when(row_label3 == "n" ~ 1,
+                           row_label3 == "Mean (SD)" ~ 2,
+                           row_label3 == "Median" ~ 3,
+                           row_label3 == "Q1, Q3" ~ 4,
+                           row_label3 == "Min, Max" ~ 5,
+                           row_label3 == "Missing" ~ 6,
                            TRUE ~ 7
-         )) %>% 
+         )) %>%
   select(-row_label1)
 
 # PFT Summary Stats -------------------------------------------------------
-pft <- adpft %>% 
-  filter(PARAMCD != "FEV1PD") %>% 
-  tplyr_table(TRTA, where= SAFFL =="Y") %>% 
-  add_total_group() %>% 
+pft <- adpft %>%
+  filter(PARAMCD != "FEV1PD") %>%
+  tplyr_table(TRTA, where= SAFFL =="Y") %>%
+  add_total_group() %>%
   add_layer(
     group_desc(AVAL, by=vars(PARAM, VISIT))
-  ) %>% 
-  get_numeric_data() %>% 
+  ) %>%
+  get_numeric_data() %>%
   mutate(col1 = as.character(col1),
-         ord1 = case_when(row_label3 =="BASELINE" ~ 1, 
+         ord1 = case_when(row_label3 =="BASELINE" ~ 1,
                           row_label3 =="WEEK 4"~ 2,
-                          row_label3 =="WEEK 8"~ 3, 
+                          row_label3 =="WEEK 8"~ 3,
                           row_label3 =="WEEK 12"~ 4),
-         ord2 =  case_when(row_label4 == "n" ~ 1, 
-                           row_label4 == "Mean (SD)" ~ 2, 
-                           row_label4 == "Median" ~ 3, 
-                           row_label4 == "Q1, Q3" ~ 4, 
-                           row_label4 == "Min, Max" ~ 5, 
-                           row_label4 == "Missing" ~ 6, 
-                           TRUE ~ 7 
+         ord2 =  case_when(row_label4 == "n" ~ 1,
+                           row_label4 == "Mean (SD)" ~ 2,
+                           row_label4 == "Median" ~ 3,
+                           row_label4 == "Q1, Q3" ~ 4,
+                           row_label4 == "Min, Max" ~ 5,
+                           row_label4 == "Missing" ~ 6,
+                           TRUE ~ 7
          ),
          row_label3 = str_to_title(row_label3)) %>%
   select(-row_label1)
 
-pft_chg <- adpft %>% 
-  filter(PARAMCD != "FEV1PD") %>% 
-  tplyr_table(TRTA, where= SAFFL =="Y") %>% 
-  add_total_group() %>% 
+pft_chg <- adpft %>%
+  filter(PARAMCD != "FEV1PD") %>%
+  tplyr_table(TRTA, where= SAFFL =="Y") %>%
+  add_total_group() %>%
   add_layer(
     group_desc(CHG, by=vars(PARAM, VISIT))
-  ) %>% 
-  get_numeric_data() %>% 
-  filter(row_label3 != "BASELINE")  %>% 
+  ) %>%
+  get_numeric_data() %>%
+  filter(row_label3 != "BASELINE")  %>%
   mutate(col1 = as.character(col1),
-         ord1 = case_when(row_label3 =="BASELINE" ~ 1, 
+         ord1 = case_when(row_label3 =="BASELINE" ~ 1,
                           row_label3 =="WEEK 4"~ 2,
-                          row_label3 =="WEEK 8"~ 3, 
+                          row_label3 =="WEEK 8"~ 3,
                           row_label3 =="WEEK 12"~ 4),
-         ord2 =  case_when(row_label4 == "n" ~ 1, 
-                           row_label4 == "Mean (SD)" ~ 2, 
-                           row_label4 == "Median" ~ 3, 
-                           row_label4 == "Q1, Q3" ~ 4, 
-                           row_label4 == "Min, Max" ~ 5, 
-                           row_label4 == "Missing" ~ 6, 
+         ord2 =  case_when(row_label4 == "n" ~ 1,
+                           row_label4 == "Mean (SD)" ~ 2,
+                           row_label4 == "Median" ~ 3,
+                           row_label4 == "Q1, Q3" ~ 4,
+                           row_label4 == "Min, Max" ~ 5,
+                           row_label4 == "Missing" ~ 6,
                            TRUE ~ 7),
          row_label3 = str_to_title(row_label3))%>%
-  select(-row_label1) 
-  
+  select(-row_label1)
+
 # AE table ----------------------------------------------------------------
 ae <- tplyr_table(adae, TRT01A) %>%
-  set_pop_data(adsl) %>% 
-  add_total_group() %>% 
+  set_pop_data(adsl) %>%
+  add_total_group() %>%
   add_layer(
-    group_count("Any Body System") %>% 
-      set_distinct_by(USUBJID) 
+    group_count("Any Body System") %>%
+      set_distinct_by(USUBJID)
   ) %>%
   add_layer(
-    group_count(target_var = vars(AEBODSYS, AETERM)) %>% 
-      set_distinct_by(USUBJID) 
-  )  %>% 
-  get_numeric_data() %>% 
+    group_count(target_var = vars(AEBODSYS, AETERM)) %>%
+      set_distinct_by(USUBJID)
+  )  %>%
+  get_numeric_data() %>%
   mutate(
-    row_label2 = if_else(is.na(row_label2), row_label1, row_label2) %>% 
+    row_label2 = if_else(is.na(row_label2), row_label1, row_label2) %>%
       str_to_title(),
-    row_label1 = str_remove(row_label1, "^\\s*") %>% 
-      str_to_title(), 
+    row_label1 = str_remove(row_label1, "^\\s*") %>%
+      str_to_title(),
     value = if_else(param == "distinct_pct", value*100, value),
     ord = row_number()
-  ) %>% 
-  group_by(row_label2) %>% 
-  mutate(ord1 = min(ord)) %>% 
-  group_by(row_label1) %>% 
-  mutate(ord2 = min(ord)) %>% 
-  ungroup() %>% 
+  ) %>%
+  group_by(row_label2) %>%
+  mutate(ord1 = min(ord)) %>%
+  group_by(row_label1) %>%
+  mutate(ord2 = min(ord)) %>%
+  ungroup() %>%
   select(-ord)
 
 
 # Stats -------------------------------------------------------------------
-adpft_mod <- adpft %>% 
-  filter(MITTFL == "Y",PARAMCD == "FEV1", VISITNUM > 2, TRTA != "Total") 
+
+
+stats_dat <- adpft %>%
+  filter(MITTFL == "Y",PARAMCD == "FEV1", VISITNUM > 2, TRTA != "Total") %>%
+  filter(!(VISIT == "WEEK 12" & USUBJID == "GSK123456-1110")) ## Drop random subject from week 12
+
+
+big_n_visit <- stats_dat %>%
+  group_by(TRTA, VISIT) %>%
+  summarize(
+    pfts = NA_character_,
+    visit = stringr::str_to_sentence(unique(VISIT)),
+    trt = unique(TRTA),
+    param = "big_n",
+    value = length(unique(USUBJID)),
+    ord1 = NA_real_,
+    .groups = "drop"
+  ) %>%
+  select(-TRTA, -VISIT)
 
 # MMRM model
 mod <- gls(CHG ~ BASE + TRTA + VISIT + BASE:VISIT + TRTA:VISIT,
-           data = adpft_mod,
+           data = stats_dat,
            # unstructured covariance matrix
            correlation = corSymm(form = ~as.numeric(VISIT)|USUBJID), # unstructured corr matrix
            weights = varIdent(form = ~1|VISIT), # allow distinct variance for each visit
            na.action = "na.omit")
 
 # Extract lsmeans and contrasts
-emm <- mod %>% emmeans(~TRTA|VISIT)  
+emm <- mod %>% emmeans(~TRTA|VISIT)
 emm_diff <- emm %>% contrast(method = "pairwise", adjust = "none")
 
 
@@ -164,25 +181,55 @@ emm_diff <- emm %>% contrast(method = "pairwise", adjust = "none")
 emm_tidy <- tidy(emm)
 emm_diff_tidy <- tidy(emm_diff, conf.int = TRUE)
 
-result_tidy <- bind_rows(emm_tidy, emm_diff_tidy) %>% 
-  mutate(pfts = "FEV1 (L)",
-         span_col = coalesce(TRTA, contrast),
-         contrast_yn = ifelse(!is.na(contrast), "y", "n")) %>% 
-  pivot_longer(cols = where(is.numeric), names_to = "param", values_to = "value") %>% 
-  filter((contrast_yn=="y" & param %in% c("estimate", "conf.low", "conf.high", "p.value")) |
-           (contrast_yn=="n" & param %in% c("estimate", "std.error"))) %>% 
-  mutate(lower_col = case_when(
-    str_detect(param, "conf") ~ "[95% CI]",
-    str_detect(span_col, " - ") & param == "estimate" ~ "Difference",
-    param == "estimate" ~ "Adjusted Mean",
-    param == "std.error" ~ "(SE)",
-    param == "p.value" ~ "p-value"),
+result_tidy <- bind_rows(emm_tidy, emm_diff_tidy) %>%
+  mutate(
+    pfts = "FEV1 (L)",
+    TRTA = coalesce(TRTA, contrast),
+    contrast_yn = ifelse(!is.na(contrast), "y", "n")
+  ) %>%
+  rename(
+    trt = TRTA,
+    visit = VISIT
+  ) %>%
+  pivot_longer(
+    cols = where(is.numeric),
+    names_to = "param",
+    values_to = "value"
+  ) %>%
+  filter(
+    (contrast_yn=="y" &
+       param %in% c("estimate", "conf.low", "conf.high", "p.value")) |
+    (contrast_yn=="n" &
+       param %in% c("estimate", "std.error"))
+  ) %>%
+  mutate(
+    measure = case_when(
+      str_detect(param, "conf") ~ "95% CI [high, low]",
+      str_detect(trt, " - ") & param == "estimate" ~ "Difference",
+      param == "estimate" ~ "Adjusted Mean",
+      param == "std.error" ~ "(SE)",
+      param == "p.value" ~ "p-value"
+    ),
+    trt = case_when(
+      str_detect(trt, " - ") ~ "GSK123456 100 mg",
+      TRUE ~ trt
+    ),
+    model_results_category = case_when(
+      measure %in% c("Adjusted Mean", "(SE)") ~ "Model Estimates",
+      measure %in% c("Difference","95% CI [high, low]", "p-value") ~ "Contrast"
+    ),
+    visit = stringr::str_to_sentence(visit),
     ord1 = case_when(
-      VISIT == "WEEK 4" ~ 1,
-      VISIT == "WEEK 8" ~ 2,
-      VISIT == "WEEK 12" ~ 3
-    )) %>% 
-  select(pfts, VISIT, span_col, lower_col, param, value, ord1)
+      measure == "Adjusted Mean" ~ 1,
+      measure == "(SE)" ~ 2,
+      measure == "Difference" ~ 3,
+      measure == "95% CI [high, low]" ~ 4,
+      measure == "p-value" ~ 5
+    )
+  ) %>%
+  select(trt, pfts, visit, model_results_category, measure, pfts, param, value, ord1) %>%
+  bind_rows(big_n_visit)
+
 
 
 
