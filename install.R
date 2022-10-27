4069b36ff4a470fd474a7c6524cf85de7694ba91
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-10-27 11:09:07 -0700

    minimum install script

---
+++ install.R
@@ -0,0 +1,26 @@
+# Install script
+
+# General Packages
+gen_pakages <- c(
+  "tidyverse",
+  "haven",
+  "gt",
+  "gtExtras",
+  "devtools",
+  "nlme",
+  "emmeans",
+  "broom"
+)
+
+#Check which packages you need
+needed_pkgs <- gen_pakages[which(!gen_pakages %in% installed.packages())]
+install.packages(needed_pkgs)
+
+library(devtools)
+
+# Pharmaverse packages
+install_github("GSK-Biostatistics/tfrmt")
+install_github("atorus-research/Tplyr@gh_issue_53_desc")
+install.packages("metacore")
+install.packages("metatools")
+install.packages("admiral")
